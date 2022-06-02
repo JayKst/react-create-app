d3d58399c6c90bfcfc8cabe0d6e3cc398d8bc096
양진규 <jkyang@spectra.co.kr>
2022-06-02 09:39:15 +0900

    rca project included typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App():JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Learn React included Typescript
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
